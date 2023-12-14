258a39c872ed3b363a5831cbef3920320dd8fc87
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-12-14 18:26:06 +0100

    build: setup passport

---
+++ src/config/passport.ts
@@ -0,0 +1,31 @@
+"use strict";
+import "dotenv/config";
+import passport from "passport";
+import passportGoogle from "passport-google-oauth";
+
+const GoogleStrategy = passportGoogle.OAuth2Strategy;
+
+const GOOGLE_CLIENT_ID: string = process.env.GOOGLE_CLIENT_ID as string;
+const GOOGLE_CLIENT_SECRET: string = process.env.GOOGLE_CLIENT_SECRET as string;
+const GOOGLE_CALLBACK_URL: string = process.env.GOOGLE_CALLBACK_URL as string;
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+passport.deserializeUser(function (obj: unknown, cb) {
+  cb(null, obj);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: GOOGLE_CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // here you can create a user in the database if you want to
+      return done(null, profile);
+    },
+  ),
+);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 "use strict";
 import cors from "cors";
 import "dotenv/config";
+import passport from "passport";
 
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import { options } from "./cors.config";
 
 const app: Express = express();
@@ -11,10 +12,7 @@ const port = process.env.PORT;
 app.use(cors(options));
 app.use(express.json());
 
-app.get("/products/:id", function (req: Request, res: Response) {
-  res.json({ msg: "This is CORS-enabled for localhost:3000!" });
-});
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(port, function () {
-  console.log(`CORS-enabled web server listening on port ${port}`);
-});
+app.listen(port);
